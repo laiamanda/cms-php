86a44d2bee5228c350f502439cf5beebf53c643a
laiamanda <40805270+laiamanda@users.noreply.github.com>
2025-10-08 10:14:50 -0400

    Create index.php

---
+++ cms/index.php
@@ -0,0 +1,53 @@
+<?php
+    require_once "partials/header.php";
+    include base_path("partials/navbar.php");
+    include base_path("partials/hero.php");
+
+    $article = new Article();
+    $articles = $article->get_all();
+?>
+
+
+
+<!-- Main Content -->
+<main class="container my-5">
+    <?php if(!empty($articles)): ?>
+        <?php foreach($articles as $articleItem): ?>
+        <!-- Blog Post 1 -->
+        <div class="row mb-4">
+            <div class="col-md-4">
+                <?php if(!empty($articleItem->image)): ?>
+                    <a href="<?php echo base_url("/cms/article.php?id=$articleItem->id"); ?>"> 
+                        <img
+                            src="<?php echo htmlspecialchars($articleItem->image); ?>"
+                            class="img-fluid"
+                            alt="Blog Post Image"
+                            style="width: 350px; height: 200px;"
+                        >
+                    </a>
+                <?php else: ?>
+                    <a href="<?php echo base_url("/cms/article.php?id=$articleItem->id"); ?>">
+                        <img
+                            src="https://placehold.co/300x250"
+                            class="img-fluid"
+                            alt="Blog Post Image"
+                            style="width: 350px; height: 200px;"
+                        >
+                    </a>
+                <?php endif; ?>
+            </div>
+            <div class="col-md-8">
+                <h2><?php echo htmlspecialchars($articleItem->title); ?></h2>
+                <p>
+                    <?php echo htmlspecialchars($article->getExcerpt($articleItem->content, 60)); ?>
+                </p>
+                <a href="article.php?id=<?php echo $articleItem->id; ?>" class="btn btn-primary">Read More</a>
+            </div>
+        </div>
+        <?php endforeach ?>
+    <?php endif; ?>
+</main>
+<?php
+    include "partials/footer.php";
+?>
+   
