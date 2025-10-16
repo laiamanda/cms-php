8784e02842f23f375ea5eeeca0f89885be80a073
laiamanda <40805270+laiamanda@users.noreply.github.com>
2025-10-16 10:38:06 -0400

    Add delete-articles.php

---
+++ cms/delete-articles.php
@@ -0,0 +1,30 @@
+<?php 
+    require 'init.php';
+
+    header('Content-Type: application/json');
+    $response = ['success' => false, 'message' => ''];
+
+    if(isPostRequest()) {
+        $data = json_decode(file_get_contents('php://input'), true);
+
+        if(isset($data['article_ids']) && is_array($data['article_ids'])) {
+            $articleIds = $data['article_ids'];
+
+            try {
+                $article = new Article();
+
+                $article->deleteMultiple($articleIds);
+                $response['success'] = true;
+            }
+            catch(Exception $e ) {
+                $response['message'] = 'ERROR ' . $e->getMessage();
+            }
+        } else {
+            $response['message'] = 'No Articles selected for deletion';
+        }
+    } else {
+        $response['message'] = 'Invalid Post Request Method';
+    }
+
+    echo json_encode($response);
+?>
